c90f82daebfdb51180a10851c8adb8f5a8d2893d
KarimovMurodilla <karimovmurodilla15@gmail.com>
2023-02-17 22:12:47 +0500

    some changes

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -15,13 +15,13 @@ async def show_profile(message: types.Message):
     user_data = await db.get_user_data(user.id)
 
     await message.answer(
-        f"<b>Сфера:</b> {' '.join([obj.name for obj in user_data.sphere])}\n"
-        f"<b>Город:</b> {' '.join([obj.name for obj in user_data.place])}\n"
+        f"<b>Сфера:</b> {', '.join([obj.name for obj in user_data.sphere])}\n"
+        f"<b>Город:</b> {', '.join([obj.name for obj in user_data.place])}\n"
         f"<b>Имя:</b> {user_data.user.name}\n"
         f"<b>Возраст:</b> {user_data.user.age}\n"
-        f"<b>Страна:</b> {' '.join([obj.name for obj in user_data.country])}\n"
-        f"<b>Занятия:</b> {' '.join([obj.emoji for obj in user_data.emoji])}\n"
-        f"<b>Интересы:</b> {' '.join([obj.name for obj in user_data.direction])}\n"
+        f"<b>Страна:</b> {', '.join([obj.name for obj in user_data.country])}\n"
+        f"<b>Занятия:</b> {', '.join([obj.emoji for obj in user_data.emoji])}\n"
+        f"<b>Интересы:</b> {', '.join([obj.name for obj in user_data.direction])}\n"
     )
 
 
