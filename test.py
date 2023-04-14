de7a3b5e8f4e0544b8f84caf03534ee6fba9ac28
KarimovMurodilla <karimovmurodilla15@gmail.com>
2023-04-14 22:27:59 +0500

    deploy to railway

--- test.py
+++ test.py
@@ -1,37 +1,12 @@
-# from loader import db
+names = []
 
+while True:
+    name = input("Enter name: ")
 
-# user_id = 875587704
-# country = 'Uzbekistan'
-# place = 'Tashkent'
+    if name == 'stop':
+        break
+    
+    else:
+        names.append(name)
 
-
-# # db.reg_country(user_id, country, place)
-# # db.del_country(user_id, country)
-
-
-# user_id = 6737735
-# country = 'USA'
-# place = 'New-York'
-
-
-# # db.reg_country(user_id, country, place)
-# # db.del_country(user_id, country)
-
-class A:
-    def _single_method(self):
-        print('_single_method')
-    def __double_method(self): # for mangling
-        print('__double_method')
-
-
-class B(A):
-    def __double_method(self): # for mangling
-        print('__double_method from inherit')
-
-
-a = A()
-a._single_method()
-
-b = B()
-# b.__double_method()
+print("Count of users: ", len(names))
